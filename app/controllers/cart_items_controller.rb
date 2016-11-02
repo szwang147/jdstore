b1b31f6f6efd63deec7da9a2d2e439498a900628
simon <wangzd.fudan@gmail.com>
2016-11-02 11:58:31 +0800

    try to delete one but fail

---
+++ app/controllers/cart_items_controller.rb
@@ -0,0 +1,13 @@
+class CartItemsController < ApplicationController
+  def destroy
+
+       @cart_items = CartItem.find(params[:id])
+       @cart_items.destroy
+       redirect_to :back
+     end
+
+
+
+
+
+end
--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,12 +3,20 @@ class CartsController < ApplicationController
       @order = Order.new
     end
 
+    def index
+      @cart = current_cart
+      @cart_items = @cart.cart_items.all
+    end
+
+
 
  def destroy
-   @cart = current_cart
+   @cart = current_cart_
    @cart.destroy
    session[:cart_id] = nil
    redirect_to :back, notice: "我要一键清空"
  end
+
+
 
  end
